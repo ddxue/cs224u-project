ec2c4b5042f4719abc0a16cc5f7d233b9e8503b9
Alexander Ping Tsun <alextsun@cs.washington.edu>
2019-05-19 13:59:25 -0700

    utils and logreg

--- util.py
+++ util.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import numpy as np
 import os
+from sentence2vec import Sentence2Vec
+from sklearn.linear_model import LogisticRegression
 
 DATA_HOME = 'Fake_News_Detection/liar_dataset'
 TEST_FILENAME = os.path.join(DATA_HOME, 'test.tsv')
 TRAIN_FILENAME = os.path.join(DATA_HOME, 'train.tsv')
 VALID_FILENAME = os.path.join(DATA_HOME, 'valid.tsv')
+MODEL_FILENAME = './job_titles.model'
 
 BINARY = '2-Way'
 HEXARY = '6-Way'
 REGRESS = 'Regress'
 MODES = [BINARY, HEXARY, REGRESS]
-MODE = MODES[2]
+MODE = MODES[0]
 
 LABEL_TO_INT = {
 	'pants-fire': 0,
@@ -30,20 +33,29 @@ def load_raw_data(file_name=TEST_FILENAME):
 	return X, y
 
 # Convert a single row of data into a vector
-def raw_to_numeric(x):
+def raw_to_numeric(x, model):
 	assert len(x) == 12
-	# TODO: do something useful
-	return np.array([1,2,3])
+	sentence_vec = model.get_vector(x[0])
+	statement_counts = x[6:11]
+	false_counts = np.array([x[6] + x[7] + x[10]])
+	true_counts = np.array([x[8] + x[9]])
+	z = np.concatenate((sentence_vec, 
+		statement_counts, 
+		false_counts, 
+		true_counts))
+	return z
 
 # Convert entire raw dataset into numeric one
-def raw_to_numeric_features(raw_X):
+def raw_to_numeric_features(raw_X, model):
 	assert raw_X.shape[1] == 12
 	X = None
 	for i in range(raw_X.shape[0]):
+		if i % 100 == 0:
+			print("On Data Point {} of {}".format(i, raw_X.shape[0]))
 		if X is None:
-			X = raw_to_numeric(raw_X[i])
+			X = raw_to_numeric(raw_X[i], model)
 		else:
-			X = np.vstack((X, raw_to_numeric(raw_X[i])))
+			X = np.vstack((X, raw_to_numeric(raw_X[i], model)))
 	return X
 
 # Convert labels to numeric.
@@ -69,9 +81,12 @@ def get_batch(X, y, batch_size=1, replace=True):
 	return X_batch, y_batch
 
 # Load data
-def load_data(file_name=TEST_FILENAME):
+def load_data(model, file_name=TEST_FILENAME):
+	print("Loading Raw Data...")
 	raw_X, y = load_raw_data(file_name)
-	X = raw_to_numeric_features(raw_X)
+	print("Converting Sentences To Vectors...")
+	X = raw_to_numeric_features(raw_X, model)
+	print("Converting Labels...")
 	y = convert_labels(y, MODE)
 	return X, y
 
@@ -87,4 +102,14 @@ def evaluate(y, yhat, mode=MODE):
 		return np.mean(np.square(y - yhat))
 
 if __name__ == '__main__':
-	load_data()
+	model = Sentence2Vec(MODEL_FILENAME)
+	train_X, train_y = load_data(model, file_name=VALID_FILENAME)
+	print(train_X.shape)
+	print(train_y.shape)
+	print("Fitting Model...")
+	log_reg = LogisticRegression().fit(train_X, train_y)
+	val_X, val_y = load_data(model, file_name=TEST_FILENAME)
+	val_yhat = log_reg.predict(val_X)
+	print(evaluate(val_y, val_yhat))
+
+
